4d71517fdca3ebfac28863e09a404a5e57ff71ca
Amei Mbah <nottlynn@gmail.com>
2017-10-03 18:03:43 -0400

    Pretty much done. Some extra features (show more) may be added later.

--- script.js
+++ script.js
@@ -2,29 +2,48 @@ $(document).ready(function() {
 
   $('#wiki-form input').keypress(function(e) {
     if (e.which == 13) {
-      $('#results').empty();
       var query = $('input').val();
-      var requestURL = 'https://en.wikipedia.org/w/api.php?action=query&format=json&srwhat=text&srprop=snippet&origin=*&list=search&continue=&srsearch=' + query;
-      var request = new XMLHttpRequest();
-      request.open('GET', requestURL);
-      request.responseType = 'json';
-      request.send();
-      request.onload = function () {
-        var queries = request.response;
-        getQueries(queries);
+      if ($.trim(query) == '') {
+        $('.no-words').addClass('appear');
+      } else {
+        $('.no-words').removeClass('appear');
+        $('#results').empty();
+        var requestURL = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&namespace=0&origin=*&search=' + query;
+        var request = new XMLHttpRequest();
+        request.open('GET', requestURL);
+        request.responseType = 'json';
+        request.send();
+        request.onload = function() {
+          var queries = request.response;
+          getQueries(queries);
+          $('#contain').addClass('slide');
+          setTimeout(function() {
+            $('#swiper').addClass('appear');
+          }, 400);
+
+        }
       }
     }
   });
+
+  $('#swiper').on('click', function() {
+    $('#swiper').removeClass('appear');
+    $('#contain').removeClass('slide');
+  });
+
 });
 
-function getQueries (jsonObj) {
-  var getQ = jsonObj['query']['search'];
+function getQueries(jsonObj) {
+  $('#results').append('<h1>Results for <b>' + jsonObj[0] + '</b>');
+
+  var getQ = jsonObj[1];
   var holder;
 
   for (var i = 0; i < getQ.length; i++) {
     holder = '<div class="result">';
-    holder += '<h2>' + getQ[i]['title'] + '</h2>';
-    holder += '<h6>' + getQ[i]['snippet'] + '</h6>';
+    holder += '<h2><a href="' + jsonObj[3][i] + '" target="_blank">' + getQ[i] +
+      '</a></h2>';
+    holder += '<h6>' + jsonObj[2][i] + '</h6>';
     holder += '</div>';
     $('#results').append(holder);
   }
